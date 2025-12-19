8900add5a66d19b79be42eb823e961fe50511009
schmeade <157747398+schmeade@users.noreply.github.com>
2025-12-19 00:55:37 -0600

    fixed lat and lon for server component

--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -3,7 +3,7 @@ export default async function LocationFinderServer() {
         const locationRes = await fetch('https://apip.cc/json');
         const locationData = await locationRes.json();
 
-        const weatherRes = await fetch('https://www.7timer.info/bin/astro.php?lon=-89.4512&lat=43.0334&ac=0&unit=metric&output=json&tzshift=0');
+        const weatherRes = await fetch('https://www.7timer.info/bin/astro.php?lon=${locationData.lon}&lat=${locationData.lat}&ac=0&unit=metric&output=json&tzshift=0');
         const weatherData = await weatherRes.json();
 
         const locationInfo = locationData;
